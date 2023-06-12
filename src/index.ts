9860bcea153eb9947debcd1b069a7220f7578bbe
kennedy.florentino <kennedyf2k@gmail.com>
2023-06-11 23:02:48 -0300

    Update tsconfig and index files

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-import { AppDataSource } from "./data-source";
 import express from "express";
-import routes from "./routes";
+import { AppDataSource } from "./data-source";
 import { errorMiddleware } from "./middlewares/error";
+import routes from "./routes";
 
 AppDataSource.initialize().then(() => {
   console.log("Data source initialized successfully!");
@@ -9,7 +9,7 @@ AppDataSource.initialize().then(() => {
   app.use(express.json());
   app.use(routes);
   app.use(errorMiddleware);
-  app.listen(process.env.PORT);
-  console.log(`App listening on port ${process.env.PORT}!`);
-  return app;
+  return app.listen(process.env.PORT, () =>
+    console.log(`App listening on port ${process.env.PORT}!`)
+  );
 });
