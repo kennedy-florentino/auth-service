f68737c30e848f7178b0b2a93152d9bdd9d0ac2c
kennedy.florentino <kennedyf2k@gmail.com>
2023-06-11 23:01:15 -0300

    Update error handlers

--- src/helpers/api-errors.ts
+++ src/helpers/api-errors.ts
@@ -1,26 +1,38 @@
+type ApiErrorAttributes = {
+  statusCode?: number;
+  message: string;
+  metadata?: unknown;
+};
+
 export class ApiError extends Error {
   public readonly statusCode: number;
+  public readonly metadata?: unknown;
 
-  constructor(message: string, statusCode: number) {
+  constructor({
+    statusCode = 500,
+    message = "Internal Server Error",
+    metadata,
+  }: ApiErrorAttributes) {
     super(message);
     this.statusCode = statusCode;
+    this.metadata = metadata;
   }
 }
 
 export class BadRequestError extends ApiError {
-  constructor(message: string) {
-    super(message, 400);
+  constructor({ message, metadata }: ApiErrorAttributes) {
+    super({ statusCode: 400, message, metadata });
   }
 }
 
 export class NotFoundError extends ApiError {
-  constructor(message: string) {
-    super(message, 404);
+  constructor({ message, metadata }: ApiErrorAttributes) {
+    super({ statusCode: 404, message, metadata });
   }
 }
 
 export class UnauthorizedError extends ApiError {
-  constructor(message: string) {
-    super(message, 401);
+  constructor({ message, metadata }: ApiErrorAttributes) {
+    super({ statusCode: 401, message, metadata });
   }
 }
